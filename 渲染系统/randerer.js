b18922e81fb099acf63afcc7ce1ad9a1dda98f2c
Nonke <codernonke@163.com>
2021-07-06 10:50:10 +0800

    渲染系统开发完成

--- 渲染系统/randerer.js
+++ 渲染系统/randerer.js
@@ -43,7 +43,6 @@ const mount = (vnode, container) => {
 const patch = (n1, n2) => {
   const el = n2.el = n1.el
   if(n1.tag === n2.tag){
-
     // 处理 props
     const newProps = n2.props
     const oldProps = n1.props
@@ -62,7 +61,6 @@ const patch = (n1, n2) => {
         }else {
           // el.setAttribute()为元素设置属性和值
           el.setAttribute(key, newValue)
-
         }
       }
     }
@@ -82,10 +80,41 @@ const patch = (n1, n2) => {
     }
 
     // 处理children
-    if(typeof n2.children === 'string'){
+    const newChildren = n2.children
+    const oldChildren = n1.children
+    if(typeof newChildren === 'string'){
       el.textContent = n2.children
-    }else {
+    //  Array.prototype.isPrototypeOf(newChildren) 检测原型链上所有的父对象是否为数组，
+    }else if(newChildren.constructor === Array) {
+      if(typeof oldChildren === 'string'){
+        // for (const item of newChildren){
+        //   console.log(item)
+        // }
+        // 先将原来的内容置空
+        el.innerHTML = ""
+        //再遍历挂载
+        newChildren.forEach((child) => {
+          mount(child, el)
+        })
 
+      }else if(oldChildren.constructor === Array){
+        // Math.min() 比较两个值，并返回最小值
+        const commonLength = Math.min(oldChildren.length, newChildren.length)
+        for(let i = 0; i < commonLength; i++){
+
+          patch(oldChildren[i], newChildren[i])
+        }
+        if(oldChildren.length < newChildren.length){
+          newChildren.slice(oldChildren.length).forEach((item) => {
+            mount(item, el)
+          })
+        }
+        if(oldChildren.length > newChildren.length){
+          oldChildren.slice(newChildren.length).forEach((item) => {
+            el.removeChild(item.el)
+          })
+        }
+      }
     }
 
 
